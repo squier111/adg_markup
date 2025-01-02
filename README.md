73d4aee622b74b306307037240e3fab402672b90
Oleg Bereza <ob@proverstka.com>
2025-01-02 14:52:05 +0100

    add gulp compiler

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-example markup for ADG project
+Markup for ADG project
+
+- install dependencies: `npm install gulp gulp-sass gulp-file-include browser-sync --save-dev`
+- html structure in this folder: `html/`
+- all variables for theme here: `scss/variables.scss`
+
+- for compiling  css/html/js/ watch changes run command: `npx gulp serve`
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const fileInclude = require("gulp-file-include");
+const browserSync = require("browser-sync").create();
+
+// Paths
+const paths = {
+  scss: "./scss/*.scss",
+  css: "./css",
+  js: "./js/*.js",
+  html: "./html/*.html",
+  dest: "./",          // Destination folder for processed HTML
+};
+
+// Compile SCSS
+function compileSass() {
+  return gulp
+    .src(paths.scss)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.stream());
+}
+
+// Process HTML with File Include
+function processHtml() {
+  return gulp
+    .src(paths.html)
+    .pipe(fileInclude({ prefix: "@@", basepath: "@file" }))
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream());
+}
+
+// Serve and Watch
+function serve() {
+  browserSync.init({
+    server: paths.dest, // Serve from the "dist" directory
+  });
+
+  gulp.watch(paths.scss, compileSass); // Watch SCSS files
+  gulp.watch(paths.html, processHtml); // Watch HTML files with partials
+  gulp.watch(paths.js).on("change", browserSync.reload); // Reload JS
+}
+
+// Tasks
+exports.sass = compileSass;
+exports.html = processHtml;
+exports.serve = gulp.series(gulp.parallel(compileSass, processHtml), serve);
